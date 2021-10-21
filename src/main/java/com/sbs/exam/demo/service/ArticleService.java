b82fb0fe30f8dc2a9e07f3ce91f78245ce875fc3
hyestar <0109jhs@naver.com>
2021-10-21 18:31:23 +0900

    ArticleService의 단순 데이터 저장 관련 로직 ArticleRepository로 옮김-Autowired주의

--- src/main/java/com/sbs/exam/demo/service/ArticleService.java
+++ src/main/java/com/sbs/exam/demo/service/ArticleService.java
@@ -1,8 +1,42 @@
 package com.sbs.exam.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.sbs.exam.demo.repository.ArticleRepository;
+import com.sbs.exam.demo.vo.Article;
 
 @Service
 public class ArticleService {
+	
+	private ArticleRepository articleRepository;
+	public List<Article> articles;
+
+	public Article writeArticle(String title, String body) {
+		
+		return articleRepository.writeArticle(title, body);
+	}
+
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		articleRepository.makeTestData();
+	}
+
+	public Article getArticle(int id) {
+		return articleRepository.getArticle(id);
+	}
+
+	public void deleteArticle(int id) {
+		articleRepository.deleteArticle(id);
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		articleRepository.modifyArticle(id, title, body);
+	}
+
+	
+
+	
 
 }
